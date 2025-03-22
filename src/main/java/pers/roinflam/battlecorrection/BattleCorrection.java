9628be90070a7003af750ef4b236ded98bc954ae
Roinflam <q2651078496@outlook.com>
2025-03-22 22:05:39 +0800

    本次提交通过在实体加入世界时正确注册所有自定义属性到玩家的属性映射中，改善了与SetBonus模组的兼容性。自定义属性现在可以被其他模组识别。 同时，修复了跳跃提升属性实现中的问题，该问题导致玩家体验重力过大和跳跃不稳定的情况。属性现在会在应用跳跃提升前正确检查已注册的实例。 这些更改使玩家能够无缝使用战斗修正(BattleCorrection)的自定义属性和套装属性(SetBonus)的装备套装功能。

--- src/main/java/pers/roinflam/battlecorrection/BattleCorrection.java
+++ src/main/java/pers/roinflam/battlecorrection/BattleCorrection.java
@@ -1,21 +1,24 @@
 package pers.roinflam.battlecorrection;
 
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import pers.roinflam.battlecorrection.attributes.*;
 import pers.roinflam.battlecorrection.proxy.CommonProxy;
 import pers.roinflam.battlecorrection.utils.Reference;
 
 import javax.annotation.Nonnull;
 
-//, dependencies = "required-after:potioncore;after:baubles"
-// dependencies = "after:baubles"
-
+@Mod.EventBusSubscriber
 @Mod(modid = Reference.MOD_ID, useMetadata = true, guiFactory = "pers.roinflam.battlecorrection.gui.ConfigGuiFactory", dependencies = "before:infinityeditor")
 public class BattleCorrection {
 
@@ -39,5 +42,46 @@ public class BattleCorrection {
 
     @Mod.EventHandler
     public static void postInit(FMLPostInitializationEvent evt) {
+    }
+
+    /**
+     * 当实体（特别是玩家）进入世界时，确保它们获得所有自定义属性
+     * 这使得SetBonus模组能够识别和修改这些属性
+     */
+    @SubscribeEvent
+    public static void onEntityJoinWorld(EntityJoinWorldEvent event) {
+        if (event.getEntity() instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) event.getEntity();
+            registerCustomAttributes(player.getAttributeMap());
+        }
+    }
+
+    /**
+     * 向实体的属性映射中添加自定义属性
+     */
+    public static void registerCustomAttributes(AbstractAttributeMap attributeMap) {
+        // 确保所有自定义属性都被添加到实体的属性映射中
+        ensureAttributeExists(attributeMap, AttributeArrowDamage.ARROW_DAMAGE);
+        ensureAttributeExists(attributeMap, AttributeMagicDamage.MAGIC_DAMAGE);
+        ensureAttributeExists(attributeMap, AttributeProjectileDamage.PROJECTILE_DAMAGE);
+        ensureAttributeExists(attributeMap, AttributeRestoreHeal.RESTORE_HEAL);
+        ensureAttributeExists(attributeMap, AttributeImmuneDamage.IMMUNE_DAMAGE);
+        ensureAttributeExists(attributeMap, AttributeBowSpeed.BOW_SPEED);
+        ensureAttributeExists(attributeMap, AttributePreparationSpeed.PREPARATION_SPEED);
+        ensureAttributeExists(attributeMap, AttributeBloodthirsty.BLOODTHIRSTY);
+        ensureAttributeExists(attributeMap, AttributeAlmightyBloodthirsty.ALMIGHTY_BLOODTHIRSTY);
+        ensureAttributeExists(attributeMap, AttributeIgnoreDamage.IGNORE_DAMAGE);
+        ensureAttributeExists(attributeMap, AttributeCriticalHitDamage.CRITICAL_HIT_DAMAGE);
+        ensureAttributeExists(attributeMap, AttributeJumpLift.JUMP_LIFT);
+        ensureAttributeExists(attributeMap, AttributeReducedFallDamage.REDUCED_FALL_DAMAGE);
+    }
+
+    /**
+     * 确保属性存在于实体的属性映射中
+     */
+    private static void ensureAttributeExists(AbstractAttributeMap attributeMap, net.minecraft.entity.ai.attributes.IAttribute attribute) {
+        if (attributeMap.getAttributeInstance(attribute) == null) {
+            attributeMap.registerAttribute(attribute);
+        }
     }
 }
--- src/main/java/pers/roinflam/battlecorrection/attributes/AttributeJumpLift.java
+++ src/main/java/pers/roinflam/battlecorrection/attributes/AttributeJumpLift.java
@@ -2,13 +2,13 @@ package pers.roinflam.battlecorrection.attributes;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.attributes.IAttribute;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.ai.attributes.RangedAttribute;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import pers.roinflam.battlecorrection.config.ConfigAttribute;
 import pers.roinflam.battlecorrection.utils.helper.task.SynchronizationTask;
-import pers.roinflam.battlecorrection.utils.util.AttributesUtil;
 
 import javax.annotation.Nonnull;
 import java.util.UUID;
@@ -23,14 +23,31 @@ public class AttributeJumpLift {
     @SubscribeEvent
     public static void onLivingJump(@Nonnull LivingEvent.LivingJumpEvent evt) {
         new SynchronizationTask(1) {
-
             @Override
             public void run() {
                 EntityLivingBase entityLivingBase = evt.getEntityLiving();
-                entityLivingBase.motionY += AttributesUtil.getAttributeValue(entityLivingBase, JUMP_LIFT, 0.42) + ConfigAttribute.jumpLift - 0.42;
-            }
 
+                // 获取注册的属性实例
+                IAttributeInstance attributeInstance = entityLivingBase.getAttributeMap().getAttributeInstance(JUMP_LIFT);
+                double jumpBoost = 0;
+
+                // 如果属性已注册，使用它的值
+                if (attributeInstance != null) {
+                    jumpBoost = attributeInstance.getAttributeValue();
+                }
+                // 否则使用原始计算方法
+                else {
+                    jumpBoost = pers.roinflam.battlecorrection.utils.util.AttributesUtil.getAttributeValue(entityLivingBase, JUMP_LIFT, 0.0);
+                }
+
+                // 添加配置中的跳跃提升值
+                jumpBoost += ConfigAttribute.jumpLift;
+
+                // 将修改后的跳跃力应用到实体
+                if (jumpBoost > 0) {
+                    entityLivingBase.motionY += jumpBoost;
+                }
+            }
         }.start();
     }
-
 }
